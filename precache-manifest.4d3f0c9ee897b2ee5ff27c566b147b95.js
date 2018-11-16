d9a6252f85a4d74ccaa48a1fd7bb4460f247348c
Rani-Chang <Yuntsye@gmail.com>
2018-11-16 09:51:41 -0800

    Updates

---
+++ precache-manifest.4d3f0c9ee897b2ee5ff27c566b147b95.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yungtc22/RWeb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "405f70dc15f050a62078",
+    "url": "/yungtc22/RWeb/static/js/runtime~main.405f70dc.js"
+  },
+  {
+    "revision": "27711be5bce9c8678d08",
+    "url": "/yungtc22/RWeb/static/js/main.27711be5.chunk.js"
+  },
+  {
+    "revision": "1e63c5d3d759c8028bf0",
+    "url": "/yungtc22/RWeb/static/js/1.1e63c5d3.chunk.js"
+  },
+  {
+    "revision": "27711be5bce9c8678d08",
+    "url": "/yungtc22/RWeb/static/css/main.6de483d6.chunk.css"
+  },
+  {
+    "revision": "1e63c5d3d759c8028bf0",
+    "url": "/yungtc22/RWeb/static/css/1.f68d9c6b.chunk.css"
+  },
+  {
+    "revision": "8494e108e1aa9245c2e5e43a72e1ad22",
+    "url": "/yungtc22/RWeb/index.html"
+  }
+];
